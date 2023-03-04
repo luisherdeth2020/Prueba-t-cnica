db9d669435d6863c9816dd7946ceef56ef0f4065
Luis Herdeth <luis.herdeth@gmail.com>
2023-03-04 10:36:19 +0100

    Update search button dimensions and render checkmark in list items when selected

--- src/App.jsx
+++ src/App.jsx
@@ -107,7 +107,7 @@ function App() {
 						/>
 
 						<div className={styles.search__button} type="submit">
-							<svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path d="M17 17L21 21" stroke="#EEF2F6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
 								<path
 									d="M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
@@ -131,7 +131,11 @@ function App() {
 												/>
 												{item.description}
 											</div>
-											{selected.some((check) => check.id === item.id) ? <div className={styles.list__check}></div> : ''}
+											{selected.some((check) => check.id === item.id) ? (
+												<div className={styles.list__check}>✔</div>
+											) : (
+												''
+											)}
 										</div>
 									</div>
 								))}
