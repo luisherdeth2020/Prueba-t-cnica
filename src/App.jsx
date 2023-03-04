7704ccc0f4dfb0f147ccc8916a01bf55c683245e
Luis Herdeth <luis.herdeth@gmail.com>
2023-03-04 10:24:31 +0100

    Update button text and SVG for search feature

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ function App() {
 							setVisible(!visible);
 						}}
 					>
-						Select your options
+						DESPLEGAR
 					</button>
 				) : selected.length > 1 ? (
 					<div>
@@ -108,17 +108,11 @@ function App() {
 
 						<div className={styles.search__button} type="submit">
 							<svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path
-									d="M17 17L21 21"
-									stroke="#EEF2F6"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-								/>
+								<path d="M17 17L21 21" stroke="#EEF2F6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
 								<path
 									d="M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
 									stroke="#EEF2F6"
-									stroke-width="2"
+									strokeWidth="2"
 								/>
 							</svg>
 						</div>
